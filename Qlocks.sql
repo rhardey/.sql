468d554f62a7c9a989fe9899250714b15b246225
Ryan Hardey <rhardey@telus.net>
2016-06-07 15:41:58 -0700

    Qlocks.sql now runnable as a script.
    Was previously just a collection of snippets without semi-colons.

--- Qlocks.sql
+++ Qlocks.sql
@@ -16,16 +16,16 @@ AND    a.hash_value = b.sql_hash_value
 AND    b.sid = c.sid 
 AND    c.owner NOT IN ('SYS','SYSTEM')
 order by a.piece
-
+;
 select a.addr,a.kaddr,a.sid,a.type,a.id1,a.id2,a.lmode,a.request,a.ctime,a.block,
        decode(a.type,'TM',(select object_name from all_objects where object_id = a.id1), '??') object
 from v$lock a
 where a.sid in (170)
-
+;
 SELECT *
 from V$LOCKED_OBJECT x
 where x.session_id = 189
-
+;
 select (select username from v$session where sid=a.sid) blocker,
         a.sid,
         ' is blocking ',
@@ -37,7 +37,7 @@ and b.request > 0
 and a.id1 = b.id1
 and a.id2 = b.id2
 and a.sid = 170
-
+;
 -- transaction = (rbs,slot,seq)
 select username,
        v$lock.sid,
@@ -49,6 +49,6 @@ select username,
 from v$lock, v$session
 where v$lock.type = 'TX'
 and v$lock.sid = v$session.sid
-and v$session.sid = 170
-
+--and v$session.sid = 170
+;
 
